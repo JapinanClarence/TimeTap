888afa8cf330af0bd415b481d0232e148a0fc203
Clarence S. Japinan <94746830+JapinanClarence@users.noreply.github.com>
2026-03-16 12:23:13 +0800

    ♻️ Added event dates on schedule card

--- resources/js/features/app/event/schedule-card.tsx
+++ resources/js/features/app/event/schedule-card.tsx
@@ -1,29 +1,28 @@
 import { Badge } from "@/components/ui/badge";
 import { EventType } from "@/types/event";
-import { formatTime12h } from "@/util/dateUtil";
+import { formatMonthDayOnly, formatSimpleDate, formatTime12h } from "@/util/dateUtil";
 import { Link } from "@inertiajs/react";
 import React from "react";
 
-interface ScheduleCardProps{
-    event: EventType,
+interface ScheduleCardProps {
+    event: EventType;
     color: {
         bg: string;
         dot: string;
-        border:string;
-        text:string;
-    },
+        border: string;
+        text: string;
+    };
 }
-export const ScheduleCard = ({event, color} : ScheduleCardProps) => {
-
+export const ScheduleCard = ({ event, color }: ScheduleCardProps) => {
     return (
         <Link key={event.id} href={`/app/schedule/${event.id}`}>
             <div
                 className={`text-xs p-3 border  rounded-xl flex justify-between shadow-xs items-center bg-white hover-card`}
             >
                 <div className="flex items-center gap-2">
-                    <div className={`w-1 mr-2`}>
-                        <div className={`${color.dot} size-2 md:h-10 md:w-full rounded-full`} />
-                    </div>
+                    <div
+                        className={`w-1 self-stretch mr-2 ${color.dot}  rounded-full`}
+                    />
                     <div>
                         <p className={`font-bold text-base`}>{event.title}</p>
                         <p className="text-muted-foreground">
@@ -32,7 +31,10 @@ export const ScheduleCard = ({event, color} : ScheduleCardProps) => {
                     </div>
                 </div>
                 <div className="text-right">
-                    <Badge className=" font-bold bg-primary/5 text-primary">
+                    <div className="text-muted-foreground ">
+                        {formatMonthDayOnly(event.start_date)} - {formatSimpleDate(event.end_date)}
+                    </div>
+                    <Badge className="font-bold bg-primary/5 text-primary">
                         {formatTime12h(event.start_time).slice(0, 5)}-
                         {formatTime12h(event.end_time)}
                     </Badge>
