d92901754f559e8a4f8fe872b0bd4ddde6e0b06c
Clarence S. Japinan <94746830+JapinanClarence@users.noreply.github.com>
2026-03-26 10:25:48 +0800

    🐛 Fixed callback redirect

--- app/Http/Controllers/Auth/GoogleAuthController.php
+++ app/Http/Controllers/Auth/GoogleAuthController.php
@@ -44,6 +44,6 @@ class GoogleAuthController extends Controller
 
         Auth::login($user);
 
-        return redirect()->intended('/dashboard');
+        return redirect()->intended('/home');
     }
 }
